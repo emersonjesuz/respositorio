fbfedb73f23538e83ea749ad941985b5c9e33b10
emerson jesuz <josiemerson2013@gmail.com>
2022-06-26 23:37:38 -0300

    atualizaçao

---
+++ site002/script1.js
@@ -0,0 +1,81 @@
+//colocar res pra todos usarem 
+var res = window.document.getElementById('res')
+var img = document.getElementById('foto')
+var body = document.getElementById('cor-body')
+var header = document.getElementById('header')
+var nav = document.getElementById('nav')
+
+function img1(){
+    
+    img.src ='imagens/IMG-20220603-WA0003.jpg'
+    body.style.background = '#251410df'
+    header.style.background ='#251410df'
+    nav.style.background ='#251410df'
+}
+
+function img2(){
+    img.src = 'imagens/IMG-20220603-WA0008.jpg'
+    body.style.background ='#935880'
+    header.style.background ='#935880'
+    nav.style.background ='#935880'
+    
+}
+
+function img3(){
+    img.src = 'imagens/IMG-20220603-WA0004.jpg'
+    body.style.background ='#656d81'
+    header.style.background ='#656d81'
+    nav.style.background ='#656d81'
+}
+
+function img4(){
+    img.src ='imagens/IMG-20220603-WA0001.jpg'
+    body.style.background ='#952130'
+    header.style.background ='#952130'
+    nav.style.background ='#952130' 
+}
+
+function img5(){
+    img.src ='imagens/IMG-20220603-WA0007.jpg'
+    body.style.background ='#225826'
+    header.style.background ='#225826'
+    nav.style.background ='#225826'
+}
+
+function img6(){
+    img.src = 'imagens/IMG-20220603-WA0011.jpg'
+    body.style.background ='#8f9a47'
+    header.style.background ='#8f9a47'
+    nav.style.background ='#8f9a47'
+}
+
+function img7(){
+    img.src = 'imagens/IMG-20220603-WA0006.jpg'
+    body.style.background ='#384d7a'
+    header.style.background ='#384d7a'
+    nav.style.background ='#384d7a'
+}
+
+
+function img8(){
+    
+    img.src ='imagens/IMG-20220603-WA0010.jpg'
+    body.style.background ='#b4b5b7 '
+    header.style.background ='#b4b5b7'
+    nav.style.background ='#b4b5b7'
+}
+
+
+function img9(){
+    img.src ='imagens/IMG-20220603-WA0002.jpg'
+    body.style.background ='#86665b '
+    header.style.background ='#86665b'
+    nav.style.background ='#86665b'
+}
+
+function img10(){
+    img.src ='imagens/IMG-20220603-WA0005.jpg'
+    body.style.background ='#abb1ed '
+    header.style.background ='#abb1ed'
+    nav.style.background ='#abb1ed'
+}
